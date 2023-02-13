1a80ca55e2ce93df4ee0f5cd2aee3d579193940f
Damian Del Percio <de.delpercio@gmail.com>
2023-02-13 10:39:58 -0300

    Add notFoundPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar.jsx';
 import ItemsListContainer from './components/ItemsListContainer/ItemsListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import PageNotFound from './views/PageNotFound/PageNotFound';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
@@ -13,7 +14,8 @@ function App() {
           <Route path='/' element={ <ItemsListContainer greeting="Hola, Damian." /> } />
           <Route path='/category/:id' element={ <ItemsListContainer /> } />
           <Route path='/detail/:id' element={ <ItemDetailContainer /> } />
-          <Route path='*' element={ <Navigate to='/' /> } />
+          <Route path="/404" element={<PageNotFound />} />
+          <Route path='*' element={ <Navigate to="/404" /> } />
         </Routes>
       </BrowserRouter>
     </div>
