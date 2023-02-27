9611c2743fa94a8e40861e0da61ca2ea2e027f78
Damian Del Percio <de.delpercio@gmail.com>
2023-02-27 12:52:01 -0300

    Add memo

--- src/components/ItemsListMap/ItemsListMap.jsx
+++ src/components/ItemsListMap/ItemsListMap.jsx
@@ -1,18 +1,20 @@
 import './ItemsListMap.css';
 import ItemsListCard from '../ItemsListCard/ItemsListCard.jsx';
+import { memo } from 'react';
 
-function ItemsListMap({items}) {
-  // Categories element
-  const itemsList = []
-
-  // Add UI for category items into categories element
-  items.map((item) => {
-    itemsList.push(
-      <ItemsListCard item={item} key={item.id}/>
-    );
-  })
-
-  return (itemsList)
-}
+const ItemsListMap = memo(({items}) => {
+    // Categories element
+    const itemsList = []
+  
+    // Add UI for category items into categories element
+    items.map((item) => {
+      itemsList.push(
+        <ItemsListCard item={item} key={item.id}/>
+      );
+    })
+  
+    return (itemsList)
+  }
+)
 
 export default ItemsListMap;
